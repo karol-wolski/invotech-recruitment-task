f4a4906787da065458a2a90b97df1418364d60b2
Karol Wolski <karolwolski04@gmail.com>
2022-07-17 17:18:14 +0200

    feat: add slider (#8)

---
+++ src/js/sliderSwiper.js
@@ -0,0 +1,10 @@
+export const slider = new Swiper(".slider", {
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
