7cfb17e6e7a42456553cc41e179fa7741801e7ca
Karol Wolski <karolwolski04@gmail.com>
2022-07-17 16:51:23 +0200

    feat: add slider

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,11 @@
 import "../style/style.scss";
 import { openNav } from "./navigation";
+import { slider } from "./sliderSwiper";
 
 const init = () => {
   const navBtn = document.querySelector("#hamburger");
   navBtn.addEventListener("click", openNav);
+  slider;
 };
 
 init();
